95470e9ad289a89beb92281ec7a96fb694556c7b
Manuel Espinoza <mfewcar@gmail.com>
2019-02-12 14:41:39 -0700

    Unit 1 Complete for turn in

---
+++ 01_HTML5_Template/js/company.js
@@ -0,0 +1,14 @@
+{
+    "@context": "http://schema.org",
+    "@type": "TravelAgency",
+    "name": "Cabot Cruises",
+    "description": "Cabot Cruises strives to deliver the best travel experience to its users",
+    "hours": "Mon-Fri 08:00-18:00",
+    "phone number": "+18017241381",
+    "address": {
+       "@type": "PostalAdress",
+       "streetAdress": "2907 Santa Monica Blvd,",
+       "addressLocality": "LSanta Monica",
+       "addressRegion": "CA"
+    },
+ }
