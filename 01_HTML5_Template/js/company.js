1fb3d586a0c4931bd382b7c35bdb8a794e53e551
Manuel Espinoza <mfewcar@gmail.com>
2019-02-12 17:53:49 -0700

    updated json

--- 01_HTML5_Template/js/company.js
+++ 01_HTML5_Template/js/company.js
@@ -1,14 +1,30 @@
+
 {
-    "@context": "http://schema.org",
-    "@type": "TravelAgency",
-    "name": "Cabot Cruises",
-    "description": "Cabot Cruises strives to deliver the best travel experience to its users",
-    "hours": "Mon-Fri 08:00-18:00",
-    "phone number": "+18017241381",
-    "address": {
-       "@type": "PostalAdress",
-       "streetAdress": "2907 Santa Monica Blvd,",
-       "addressLocality": "LSanta Monica",
-       "addressRegion": "CA"
-    },
+   "@context": "http://schema.org",
+   "@type": "Cruise Line",
+   "address": {
+     "@type": "PostalAddress",
+     "addressLocality": "Miami",
+     "addressRegion": "FL",
+     "postalCode": "33410",
+     "streetAddress": "500 South Collins Avenue Suite 100"
+   },
+   "name": "Cabot Cruises",
+   "openingHours": [
+     "Mo-Fr 09:00-16:30"
+   ],
+   "priceRange": "$$",
+   "servesCarribean": [
+     "Aruba", 
+     "Bahamas",
+     "Barbados",
+     "Cayman Islands",
+     "Domincan Republic",
+     "Jamaica",
+     "Martinique",
+     "St Lucia",
+     "Trinidad and Tobago"
+   ],
+   "telephone": "(866) 577-1000",
+   "url": "http://www.cabotcruises.com"
  }
