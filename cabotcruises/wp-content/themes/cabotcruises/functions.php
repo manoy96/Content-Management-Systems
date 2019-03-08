f9f67b06b4a7b659e3e45d60ecc657e358687229
manoy96 <mfewcar@gmail.com>
2019-03-07 22:18:36 -0700

    commented out read more function. Not working properly yet

--- cabotcruises/wp-content/themes/cabotcruises/functions.php
+++ cabotcruises/wp-content/themes/cabotcruises/functions.php
@@ -5,4 +5,9 @@ add_filter('user_can_richedit','_return_false',50);
 // create a menu
 register_nav_menu('siteNavigation', 'Primary Navigation')
 
-?>
+// Read More Button
+// function wpdocs_excerpt_more( $more ) {
+//     return '<a href="'.get_the_permalink().'" rel="nofollow">Read More...</a>';
+// }
+// add_filter( 'excerpt_more', 'wpdocs_excerpts_more' );
+?> 
