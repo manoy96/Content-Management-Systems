8773110d2df39c20e3f878ce22ee7c2e1b251f21
manoy96 <mfewcar@gmail.com>
2019-03-11 00:10:58 -0600

    edits to read more functions, and styles.

--- cabotcruises/wp-content/themes/cabotcruises/functions.php
+++ cabotcruises/wp-content/themes/cabotcruises/functions.php
@@ -7,6 +7,8 @@ register_nav_menu('siteNavigation', 'Primary Navigation');
 
 // Read More Button
 function wpdocs_excerpt_more($more){
-   return '<a href="' . get_the_permalink() . '" rel="nofollow">Read More...</a>';
+   return '<a href="' . get_the_permalink() . '" rel="nofollow" class="read-more">Read More...</a>';
 }
 add_filter('excerpt_more', 'wpdocs_excerpts_more');
+
+?>
